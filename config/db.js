90f83e812331568243224ec89cede810b9f892d5
Xamuelmadu <coolcatinvite@gmail>
2026-03-31 10:08:59 +0100

    fix: remove deprecated mongoose options

--- config/db.js
+++ config/db.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    console.log("Connecting to MongoDB...");
+
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB connected");
   } catch (error) {
